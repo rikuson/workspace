a51eee2bdb619fc72b7911c2c0392f0d33818bc6
Rikson <rikuson@users.noreply.github.com>
2022-05-05 03:34:26 +0900

    Add native apps

---
+++ provision/roles/native/spec/native_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+[
+  'clipy',
+  'docker',
+  'drawio',
+  'dynalist',
+  'font-fira-code-nerd-font',
+  'google-chrome',
+  'mas',
+  'resilio-sync',
+  'spotify',
+  'typora',
+].each do |item|
+  describe command("brew info #{item}") do
+    its(:exit_status) { should eq 0 }
+  end
+end
+
+[
+  { 'name' => 'BetterSnapTool', 'id' => 417375580 },
+  { 'name' => 'Evernote', 'id' => 406056744 },
+  { 'name' => 'Kindle', 'id' => 405399194 },
+  { 'name' => 'Logic Pro X', 'id' => 634148309 },
+  { 'name' => 'Microsoft Remote Desktop', 'id' => 1295203466 },
+  { 'name' => 'PDFOutliner', 'id' => 420874236 },
+  { 'name' => 'Save to Pocket', 'id' => 1477385213 },
+  { 'name' => 'Spark', 'id' => 1176895641 },
+  { 'name' => 'Xcode', 'id' => 497799835 },
+  { 'name' => 'Keynote', 'id' => 409183694 },
+].each do |item|
+  describe command("mas info #{item['id']}") do
+    its(:exit_status) { should eq 0 }
+  end
+end
