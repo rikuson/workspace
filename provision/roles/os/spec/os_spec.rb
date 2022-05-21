bc76eeeed3650378cbdeeb7cfa4fcac226839243
Rikson <rikuson@users.noreply.github.com>
2022-05-21 21:42:01 +0900

    Fix ci (#10)
    * Fix ci

--- provision/roles/os/spec/os_spec.rb
+++ provision/roles/os/spec/os_spec.rb
@@ -12,38 +12,23 @@ describe command('defaults read com.apple.AppleMultitouchTrackpad Clicking') do
   its(:stdout) { should match '1' }
 end
 describe command('defaults read com.apple.finder AppleShowAllFiles') do
-  its(:stdout) { should match '1' }
+  its(:stdout) { should match 'True' }
 end
 describe command('defaults read com.apple.dock show-recents') do
   its(:stdout) { should match '0' }
 end
 describe command('defaults read com.apple.desktopservices DSDontWriteNetworkStores') do
-  its(:stdout) { should match '1' }
+  its(:stdout) { should match 'True' }
 end
 describe command('defaults read com.apple.dock autohide') do
   its(:stdout) { should match '1' }
 end
 describe command('defaults read com.apple.menuextra.battery ShowPercent') do
-  its(:stdout) { should match 'yes' }
-end
-describe command('defaults read com.apple.Safari IncludeInternalDebugMenu') do
-  its(:stdout) { should match 'true' }
+  its(:stdout) { should match 'True' }
 end
 describe command('defaults read com.apple.finder ShowPathbar') do
-  its(:stdout) { should match 'true' }
+  its(:stdout) { should match '1' }
 end
 describe command('defaults read com.apple.spaces spans-displays') do
-  its(:stdout) { should match 'true' }
+  its(:stdout) { should match '1' }
 end
-describe command('defaults read com.apple.screensaver idleTime') do
-  its(:stdout) { should match '0' }
-end
-// describe command('defaults read com.apple.Finder QLHidePanelOnDeactivate') do
-//   its(:stdout) { should match '0' }
-// end
-// describe command('defaults read com.apple.finder QLEnableTextSelection') do
-//   its(:stdout) { should match '1' }
-// end
-// describe command('defaults read -g NSAutomaticSpellingCorrectionEnabled') do
-//   its(:stdout) { should match '0' }
-// end
