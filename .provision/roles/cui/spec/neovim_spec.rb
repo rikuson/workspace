eec17d5ac2ebb73e6238b7ab64b1d6ebeeccf5c3
Riku Takeuchi <rikuson@users.noreply.github.com>
2022-09-25 11:10:14 +0900

    feat: Add neovim devicon pluguin

--- .provision/roles/cui/spec/neovim_spec.rb
+++ .provision/roles/cui/spec/neovim_spec.rb
@@ -1,10 +1,31 @@
 home_path = Specinfra.backend.run_command('echo $HOME').stdout.strip
 
+# vaffle.vim
+[
+  { 'namespace' => 'cocopon', 'name' => 'vaffle.vim' },
+  { 'namespace' => 'ryanoasis', 'name' => 'vim-devicons' },
+].each do |item|
+  describe file("#{home_path}/.local/share/nvim/site/pack/#{item['namespace']}/start/#{item['name']}") do
+    it { should be_directory }
+  end
+end
+
+# lightline.vim
+[
+  { 'namespace' => 'itchyny', 'name' => 'lightline.vim' },
+  { 'namespace' => 'ryanoasis', 'name' => 'vim-devicons' },
+].each do |item|
+  describe file("#{home_path}/.local/share/nvim/site/pack/#{item['namespace']}/start/#{item['name']}") do
+    it { should be_directory }
+  end
+end
+
 # telescope.nvim
 describe package('ripgrep') do
   it { should be_installed.by('homebrew') }
 end
 [
+  { 'namespace' => 'kyazdani42', 'name' => 'nvim-web-devicons' },
   { 'namespace' => 'nvim-lua', 'name' => 'plenary.nvim'  },
   { 'namespace' => 'nvim-telescope', 'name' => 'telescope.nvim' },
 ].each do |item|
