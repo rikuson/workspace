16c1510a8b9bd5e818781ca9d62649e5641e8029
Riku Takeuchi <rikuson@users.noreply.github.com>
2023-05-08 01:10:08 +0900

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,24 @@ if [[ -z "$WORKSPACE_REPOSITORY_URL" ]]; then
   abort '`WORKSPACE_REPOSITORY_URL` is undefined.'
 fi
 
-# Install homebrew
-/bin/bash -c `curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh`
-if [[ -d /opt/homebrew ]]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+# Mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Install homebrew
+  /bin/bash -c `curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh`
+  if [[ -d /opt/homebrew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+  brew update
+  brew upgrade
+  brew install ansible
 fi
-brew update
-brew upgrade
+
+# WSL
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  sudo apt-get -y update
+  sudo apt-get -y upgrade
+  sudo apt-get install -y git ansible python p7zip-full
+fi
 
 # Create bare repository
 mkdir $HOME/workspace
@@ -35,7 +46,6 @@ config branch --set-upstream-to=origin/master
 sh -c `curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh`
 
 # Initialize
-brew install ansible
 gem install bundler ansible_spec
 
 touch $HOME/.zprofile
