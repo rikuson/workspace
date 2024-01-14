b783edcbac3e70ebb8f05995f94bba0a3285dc53
Riku Takeuchi <rikuson@users.noreply.github.com>
2024-01-14 18:57:17 +0900

    feat: replace vaffle to file_browser

--- roles/cui/templates/.config/nvim/lua/plugin/telescope.lua
+++ roles/cui/templates/.config/nvim/lua/plugin/telescope.lua
@@ -6,6 +6,11 @@ require('telescope').setup{
       }
     }
   },
+  extensions = {
+    file_browser = {
+      hijack_netrw = true,
+    }
+  }
 }
 vim.keymap.set('n', '<leader>f', '<cmd>Telescope git_files<cr>')
 vim.keymap.set('n', '<Leader>s', '<cmd>Telescope git_status<cr>')
@@ -13,4 +18,6 @@ vim.keymap.set('n', '<leader>b', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>g', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>h', '<cmd>Telescope oldfiles<cr>')
 vim.keymap.set('n', '<leader>z', '<cmd>LazyGit<cr>')
-vim.keymap.set('n', '<leader>.', '<cmd>Telescope file_browser<cr>')
+vim.keymap.set('n', '<leader>.', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>')
+
+require("telescope").load_extension "file_browser"
