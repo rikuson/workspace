cb2100a51eb3896620ee7bd2432b97c4f17b20d7
Riku Takeuchi <rikuson@users.noreply.github.com>
2024-01-31 22:49:18 +0900

    feat: udpate nvim-cmp keymap

--- roles/cui/templates/.config/nvim/lua/plugin/nvim-cmp.lua
+++ roles/cui/templates/.config/nvim/lua/plugin/nvim-cmp.lua
@@ -13,10 +13,6 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true
-    }),
   }),
   sources = cmp.config.sources({
     { name = 'copilot' },
