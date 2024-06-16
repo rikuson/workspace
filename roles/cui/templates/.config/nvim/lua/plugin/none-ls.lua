07c70a603026a8a9c87f7b082968656157e3a675
Riku Takeuchi <rikuson@users.noreply.github.com>
2024-06-16 15:25:51 +0900

    fix: filter linter on save to fix (#115)

--- roles/cui/templates/.config/nvim/lua/plugin/none-ls.lua
+++ roles/cui/templates/.config/nvim/lua/plugin/none-ls.lua
@@ -1,5 +1,30 @@
 local null_ls = require('null-ls')
+
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- Fix on save
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
+end
+
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint,
@@ -7,19 +32,7 @@ null_ls.setup({
     null_ls.builtins.diagnostics.rubocop,
     null_ls.builtins.formatting.rubocop,
   },
-  -- Fix on save
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end
-  end,
+  on_attach = on_attach,
 })
 
 require('mason-null-ls').setup({
