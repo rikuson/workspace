8bddac05e0b6f02e7a6bb77fad038939293211cc
Riku Takeuchi <rikuson@users.noreply.github.com>
2024-06-30 18:58:52 +0900

    feat: enable to toggle term

--- roles/cui/templates/.config/nvim/lua/plugin/toggleterm.lua
+++ roles/cui/templates/.config/nvim/lua/plugin/toggleterm.lua
@@ -21,3 +21,4 @@ end
 
 vim.api.nvim_set_keymap("n", "<leader>z", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua _lazydocker_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
