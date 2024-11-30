c8fed07d6b52565d44f323c0a339dc50f464ac0f
Riku Takeuchi <rikuson@users.noreply.github.com>
2024-12-01 01:54:20 +0900

    fix: workaround for eslint_d

--- roles/cui/templates/.config/nvim/lua/plugins/none-ls.lua
+++ roles/cui/templates/.config/nvim/lua/plugins/none-ls.lua
@@ -33,7 +33,7 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.eslint_d,
+        require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
