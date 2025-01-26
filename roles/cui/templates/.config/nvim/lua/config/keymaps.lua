229a6796530c23937bed4ae4f458b28ec82dc819
Riku Takeuchi <rikuson@users.noreply.github.com>
2025-01-26 15:51:43 +0900

    feat: udpate vim settings

--- roles/cui/templates/.config/nvim/lua/config/keymaps.lua
+++ roles/cui/templates/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<Cr>", "A<Cr><Esc>", { noremap = true })
+
+-- Insert mode key mappings
+vim.keymap.set("i", "<C-a>", "<C-o>^", { noremap = true })
+vim.keymap.set("i", "<C-b>", "<C-g>u<Left>", { noremap = true })
+vim.keymap.set("i", "<C-f>", "<C-g>u<Right>", { noremap = true })
+vim.keymap.set("i", "<C-e>", "<C-g>u<End>", { noremap = true })
+vim.keymap.set("i", "<C-y>", '<C-r>"', { noremap = true })
+vim.keymap.set("i", "<C-p>", "<C-o>gk", { noremap = true })
+vim.keymap.set("i", "<C-n>", "<C-o>gj", { noremap = true })
+vim.keymap.set("i", "<C-k>", "<Esc>ld$a", { noremap = true })
+vim.keymap.set("i", "<C-d>", "<Del>", { noremap = true })
+vim.keymap.set("i", "<C-h>", "<Bs>", { noremap = true })
+
+-- Command-line mode key mappings
+vim.keymap.set("c", "<C-d>", "<Del>", { noremap = true })
+vim.keymap.set("c", "<C-h>", "<Bs>", { noremap = true })
+vim.keymap.set("c", "<C-a>", "<Home>", { noremap = true })
+vim.keymap.set("c", "<C-f>", "<Right>", { noremap = true })
+vim.keymap.set("c", "<C-b>", "<Left>", { noremap = true })
+vim.keymap.set("c", "<C-p>", "<Up>", { noremap = true })
+vim.keymap.set("c", "<C-n>", "<Down>", { noremap = true })
---
+++ roles/cui/templates/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,83 @@
+return {
+	{ "L3MON4D3/LuaSnip" },
+	{ "RRethy/vim-illuminate" },
+	{ "andymass/vim-matchup" },
+	{ "editorconfig/editorconfig-vim" },
+	{ "folke/trouble.nvim" },
+	{ "folke/which-key.nvim" },
+	{ "hashivim/vim-terraform" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-path" },
+	{ "itchyny/lightline.vim" },
+	{ "jay-babu/mason-null-ls.nvim" },
+	{ "kyazdani42/nvim-web-devicons" },
+	{
+    "lewis6991/gitsigns.nvim",
+    config = true,
+  },
+	{
+	  "lukas-reineke/indent-blankline.nvim",
+	  main = "ibl",
+	  ---@module "ibl"
+	  ---@type ibl.config
+	  opts = {
+	    indent = { char = '¦' }
+	  },
+	},
+	{
+    "machakann/vim-highlightedyank",
+    config = function()
+      vim.g.highlightedyank_highlight_duration = 500
+    end,
+  },
+	{ "mbbill/undotree" },
+	{ "mg979/vim-visual-multi" },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "LspAttach",
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      vim.diagnostic.config({ virtual_text = false })
+      require('tiny-inline-diagnostic').setup({
+        options = {
+          multilines = true,
+        },
+      })
+    end
+  },
+  { "j-hui/fidget.nvim" },
+	{ "nvim-lua/plenary.nvim" },
+	{ "nvim-telescope/telescope-file-browser.nvim" },
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "onsails/lspkind-nvim" },
+	{ "psliwka/vim-smoothie" },
+	{
+    "rebelot/kanagawa.nvim",
+    config = function()
+      vim.cmd [[colorscheme kanagawa-dragon]]
+    end,
+  },
+	{ "statianzo/vim-jade" },
+	{
+    "thinca/vim-quickrun",
+    config = function()
+      if vim.version().major > 8 or (vim.version().major == 8 and vim.version().minor >= 2) then
+        vim.g.quickrun_config = {
+          _ = {
+            outputter = "popup"
+          }
+        }
+      end
+      vim.g.quickrun_no_default_key_mappings = 1
+    end
+  },
+	{ "tpope/vim-abolish" },
+	{ "tpope/vim-fugitive" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "williamboman/mason.nvim" },
+	{ "windwp/nvim-ts-autotag" },
+	{ "zbirenbaum/copilot-cmp" },
+	{ "zbirenbaum/copilot.lua" },
+}
