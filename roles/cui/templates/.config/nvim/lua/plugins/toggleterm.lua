9f10ae6d804153a5c0995b89f507902cff57babb
Riku Takeuchi <rikuson@users.noreply.github.com>
2025-11-28 14:53:14 +0900

    feat: open terminal on floating window

--- roles/cui/templates/.config/nvim/lua/plugins/toggleterm.lua
+++ roles/cui/templates/.config/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
   keys = {
     { "<leader>gz", "<cmd>lua _lazygit_toggle()<CR>", desc = "Lazygit", mode = "n" },
     { "<leader>dz", "<cmd>lua _lazydocker_toggle()<CR>", desc = "Lazydocker", mode = "n" },
-    { "<leader>t", "<cmd>ToggleTerm<CR>", desc = "Terminal", mode = "n" },
+    { "<leader>t", "<cmd>lua _floatterm_toggle()<CR>", desc = "Terminal", mode = "n" },
   },
   config = function()
     require("toggleterm").setup()
@@ -25,6 +25,18 @@ return {
     })
     function _lazydocker_toggle()
       lazydocker:toggle()
+    end
+
+    local floatterm = Terminal:new({
+      direction = "float",
+      hidden = true,
+      float_opts = {
+        width = 100,
+        height = 30,
+      }
+    })
+    function _floatterm_toggle()
+      floatterm:toggle()
     end
   end
 }
